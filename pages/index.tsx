87bc28f22d7953e929e5b858213d0c64b5bb90b5
Grae-Saunders <60531598+Grae-Saunders@users.noreply.github.com>
2023-06-01 08:01:01 +0000

    Midway module 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import {Button, Stack, TextField, Typography } from "@mui/material";
+import { Typography, Stack, TextField, Button, Card, CardContent } from "@mui/material";
+import { useRef, useCallback, useState } from "react";
 
 export default function Home() {
-  function generateBlurb(): void {
-        throw new Error("Function not implemented.");
-      }
+  const blurbRef = useRef("");
+  const [generatingPosts, setGeneratingPosts] = useState("");
+  const generateBlurb = useCallback(async () => {
+    const response = await fetch("/api/generateBlurb", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: blurbRef.current,
+      }),
+    });
+    if (!response.ok) {
+         throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log("Response was:", JSON.stringify(data));
+    setGeneratingPosts(data.choices[0].message.content);
+  }
+  , [blurbRef.current]);
   return (
     <Stack
       component="main"
@@ -25,10 +43,18 @@ export default function Home() {
        multiline
        fullWidth
        minRows={4}
+       onChange={(e) => {
+         blurbRef.current = e.target.value;
+       }}
        sx={{ "& textarea": { boxShadow: "none !important" } }}
        placeholder="Key words on what you would like your blurb to be about"
      ></TextField>
      <Button onClick={generateBlurb}>Generate Blurb</Button>
+     {generatingPosts && (
+    <Card>
+      <CardContent>{generatingPosts}</CardContent>
+    </Card>
+ )}
    </Stack>
   );
 }
